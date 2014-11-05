30318bcd0f5fe5691f6b81a482df624c33161815
nikolaigauss <sammet.90@hotmail.com>
2014-11-05 01:59:16 -0300

    Agregada lógica inscripción a partido.

--- OrganizadorFutbol5/Ventanas/HomeJugador.Designer.cs
+++ OrganizadorFutbol5/Ventanas/HomeJugador.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.standard = new System.Windows.Forms.RadioButton();
+            this.solidaria = new System.Windows.Forms.RadioButton();
+            this.condicional = new System.Windows.Forms.RadioButton();
+            this.inscribirse = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,17 +41,63 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(323, 177);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.inscribirse});
+            this.dataGridView1.Location = new System.Drawing.Point(151, 177);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(412, 150);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // standard
+            // 
+            this.standard.AutoSize = true;
+            this.standard.Checked = true;
+            this.standard.Location = new System.Drawing.Point(323, 69);
+            this.standard.Name = "standard";
+            this.standard.Size = new System.Drawing.Size(120, 17);
+            this.standard.TabIndex = 1;
+            this.standard.TabStop = true;
+            this.standard.Text = "Inscripcion standard";
+            this.standard.UseVisualStyleBackColor = true;
+            // 
+            // solidaria
+            // 
+            this.solidaria.AutoSize = true;
+            this.solidaria.Location = new System.Drawing.Point(323, 92);
+            this.solidaria.Name = "solidaria";
+            this.solidaria.Size = new System.Drawing.Size(117, 17);
+            this.solidaria.TabIndex = 2;
+            this.solidaria.TabStop = true;
+            this.solidaria.Text = "Inscripcion solidaria";
+            this.solidaria.UseVisualStyleBackColor = true;
+            // 
+            // condicional
+            // 
+            this.condicional.AutoSize = true;
+            this.condicional.Location = new System.Drawing.Point(323, 115);
+            this.condicional.Name = "condicional";
+            this.condicional.Size = new System.Drawing.Size(133, 17);
+            this.condicional.TabIndex = 3;
+            this.condicional.TabStop = true;
+            this.condicional.Text = "Inscripcion condicional";
+            this.condicional.UseVisualStyleBackColor = true;
+            // 
+            // inscribirse
+            // 
+            this.inscribirse.HeaderText = "Inscribirse";
+            this.inscribirse.Name = "inscribirse";
+            this.inscribirse.ReadOnly = true;
             // 
             // HomeJugador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(575, 339);
+            this.Controls.Add(this.condicional);
+            this.Controls.Add(this.solidaria);
+            this.Controls.Add(this.standard);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -58,11 +108,16 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.HomeJugador_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton standard;
+        private System.Windows.Forms.RadioButton solidaria;
+        private System.Windows.Forms.RadioButton condicional;
+        private System.Windows.Forms.DataGridViewButtonColumn inscribirse;
     }
 }
--- OrganizadorFutbol5/Ventanas/HomeJugador.cs
+++ OrganizadorFutbol5/Ventanas/HomeJugador.cs
@@ -13,19 +13,54 @@ namespace OrganizadorFutbol5.Ventanas
     public partial class HomeJugador : Form
     {
         Commons commons = new Commons();
-        
+
+        int jugadorID;
+        DataBaseDataContext db = new DataBaseDataContext();
+
         public HomeJugador(int id)
         {
+            jugadorID = id;
+
             InitializeComponent();
 
-            DataBaseDataContext db = new DataBaseDataContext();
-
             
+
+            var partidos = from x in db.Partidos
+                               select new {x.ID , x.Fecha };
+
+            dataGridView1.DataSource = partidos;
+
         }
 
         private void HomeJugador_FormClosing(object sender, FormClosingEventArgs e)
         {
             e.Cancel = commons.preguntaCierreSesion();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Inscripcion inscripccion = new Inscripcion();
+            inscripccion.Fecha = System.DateTime.Now;
+            inscripccion.JugadorID = jugadorID;
+            inscripccion.PartidoID = (decimal) dataGridView1.Rows[e.RowIndex].Cells["ID"].Value;
+            if (standard.Checked)
+                inscripccion.Prioridad = 1;
+            if (solidaria.Checked)
+                inscripccion.Prioridad = 2;
+            else
+                inscripccion.Prioridad = 3;
+
+
+            Partido partido = (from p in db.Partidos
+                              where p.ID == inscripccion.PartidoID
+                              select p).First();
+            partido.inscribir(inscripccion);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
